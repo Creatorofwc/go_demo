e4cd045c9ed17d7eb6b7edb4fdbc3d730e2f6fc4
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2020-07-08 23:54:46 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func main() {
+	url := "http://hello-world-demo.ir-e1.cloudhub.io/api/helloworld"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<p>Welcome to Demo Application!</p>")
+		fmt.Fprintf(w, "<p>Server time:"+time.Now().Format("20060102150405")+"</p>")
+		fmt.Fprintf(w, "<p>Server OS:"+runtime.GOOS+"<p>")
+		fmt.Fprintf(w, "<p>Mulesoft Response:"+getMulesoft(url)+"</p>")
+	})
+	http.ListenAndServe(":80", nil)
+}
+
+func getMulesoft(url string) string {
+	r, err := myClient.Get(url)
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	defer r.Body.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+
+	return buf.String()
+}
