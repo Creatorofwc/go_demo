c927ac1150534d0a82efae2624a3a5e006f10bbe
Vadym Kovalenko <vadym.kovalenko@gmail.com>
2020-07-09 11:16:35 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,53 +1,27 @@
-// main.go
-
 package main
 
 import (
-	"encoding/json"
-	"html/template"
-	"time"
-        "fmt"
-	"io/ioutil"
-	"log"
 	"bytes"
+	"fmt"
 	"net/http"
+	"runtime"
+	"time"
 )
 
-var t *template.Template
-var c config
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-type data struct {
-	Weekday  time.Weekday
-	Greeting string
-}
-
-type config struct {
-	Greeting string
-}
-
 func main() {
-	loadConfig()
-	initializeTemplates()
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":80", nil))
+	url := "http://hello-world-demo.ir-e1.cloudhub.io/api/helloworld"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<p>Welcome to Demo Application!</p>")
+		fmt.Fprintf(w, "<p>Server time:"+time.Now().Format("20060102150405")+"</p>")
+		fmt.Fprintf(w, "<p>Server OS:"+runtime.GOOS+"<p>")
+		fmt.Fprintf(w, "<p>Mulesoft Response:"+getMulesoft(url)+"</p>")
+	})
+	http.ListenAndServe(":80", nil)
 }
 
-func loadConfig() {
-	contentBytes, _ := ioutil.ReadFile("config.json")
-	json.Unmarshal(contentBytes, &c)
-}
-
-func initializeTemplates() {
-	t, _ = template.ParseFiles("index.html")
-}
-func handler(w http.ResponseWriter, r *http.Request) {
-
-//        url := "http://hello-world-demo.ir-e1.cloudhub.io/api/helloworld"
-	data := &data{time.Now().Weekday(), c.Greeting}
-
-	t.Execute(w, data)
-}
 func getMulesoft(url string) string {
 	r, err := myClient.Get(url)
 	if err != nil {
